131e030530c16c885349bba3e7553b58aaa1c890
J-Mara <jmaragakis20@stuy.edu>
2020-11-03 20:11:08 -0500

    first form of SuperArray, missing some aspects

---
+++ SuperArray.java
@@ -0,0 +1,30 @@
+public class SuperArray{
+  private String [] data;
+  private int size;
+
+  public SuperArray(){
+    size = 0;
+    data = new String[10];
+  }
+
+  public int size(){
+    return size;
+  }
+
+  public boolean add(String element){
+    data[size] = element;
+    size += 1;
+    return true;
+  }
+
+  public String get(int index){
+    return data[index];
+  }
+
+  public String set(int index, String element){
+    String holder = data[index];
+    data[index] = element;
+    return holder;
+  }
+
+}
