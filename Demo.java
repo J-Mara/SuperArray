73633f540546ef0bf9cb895195973bc4b25d67a1
J-Mara <jmaragakis20@stuy.edu>
2020-11-09 18:01:16 -0500

    Demo with findOverlap test in main

--- Demo.java
+++ Demo.java
@@ -20,6 +20,7 @@ public class Demo{
 
   public static void main(String[] args) {
     SuperArray words = new SuperArray();
+    SuperArray words2 = new SuperArray();
     words.add("kani");
     words.add("uni");
     words.add("ebi");
@@ -31,8 +32,17 @@ public class Demo{
     words.add("una");
     words.add("ebi");
     words.add("toro");
+    words2.add("noop");
+    words2.add("kani");
+    words2.add("noop");
+    words2.add("kani");
+    words2.add("una");
+    words2.add("ebi");
+    words2.add("ebi");
     System.out.println(words);
     removeDuplicates(words);
     System.out.println(words);
+    System.out.println(words2);
+    System.out.println(findOverlap(words, words2).toString());
   }
 }
