2b1dd603efdfc4fe1a1ebbd06c48ae61c3e6882b
J-Mara <jmaragakis20@stuy.edu>
2020-11-09 17:38:53 -0500

    Demo with main

--- Demo.java
+++ Demo.java
@@ -6,4 +6,21 @@ public class Demo{
       }
     }
   }
+  public static void main(String[] args) {
+    SuperArray words = new SuperArray();
+    words.add("kani");
+    words.add("uni");
+    words.add("ebi");
+    words.add("una");
+    words.add("una");
+    words.add("ebi");
+    words.add("kani");
+    words.add("una");
+    words.add("una");
+    words.add("ebi");
+    words.add("toro");
+    System.out.println(words);
+    removeDuplicates(words);
+    System.out.println(words);
+  }
 }
