d25e8908fd00468a7a462248d8fb55aa4352b0a0
J-Mara <jmaragakis20@stuy.edu>
2020-11-12 21:44:00 -0500

    final

--- SuperArray.java
+++ SuperArray.java
@@ -59,6 +59,9 @@ public class SuperArray{
   }
 
   public String remove(int index){
+    if(index < 0 || index > size){
+      throw new IndexOutOfBoundsException("index " + index + " is out of bounds.");
+    }
     String holder = data[index];
     for(int i = index; i < size; i++){
       data[i] = data[i + 1];
